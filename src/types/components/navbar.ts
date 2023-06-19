bcdde0572b972bf9f2e2e7015cc1ed3d15865cdc
Taylor-2t9 <ftayloros29@gmail.com>
2023-06-19 20:54:27 -0300

    Correcting Navbar Types

--- src/types/components/navbar.ts
+++ src/types/components/navbar.ts
@@ -2,5 +2,5 @@ import { Dispatch, SetStateAction } from "react";
 import { PictureData } from "../api/apod";
 
 export interface NavProps {
-    setPicture: Dispatch<SetStateAction<PictureData>>
+    setPicture?: Dispatch<SetStateAction<PictureData>>
 }
