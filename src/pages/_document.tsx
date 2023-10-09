336d22289141b461c071a604e4cade70c75597f7
Taylor-2t9 <ftayloros29@gmail.com>
2023-10-08 22:52:24 -0300

    Remove autotranslation in html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" translate="no">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"></link>
         <link rel="icon" href="/favicon.ico" />
